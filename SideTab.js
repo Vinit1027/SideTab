99ed358ffd37abb965762a95488d3d06960ee5bb
Vinit More <viniot1027@gmail.com>
2024-04-17 20:54:38 +0530

    initial version

---
+++ SideTab.js
@@ -0,0 +1,19 @@
+function offcanvasOpen(){
+    document.getElementById('canva').className = "yolo";
+}
+
+
+function offcanvasClose(){
+    document.getElementById('canva').className = " totalcanvas";
+
+}
+
+
+function offclose(){
+    document.getElementById('canva').className = " totalcanvas";
+}
+
+
+function drpdown(){
+    document.getElementById('drpdowne').className = "menuvisi";
+}
